23c82ff736916d60ef08ed9749d80d6b835b72c5
JUnzuetaLuza <joseunz2k@gmail.com>
2024-08-25 14:08:46 -0500

    fix(Inicio): Carrousel background fixed

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -31,9 +31,23 @@ export const Carrousel = () => {
     const prevSlide = () => {
         showSlide(currentSlide - 1);
     }
-    
+
+    const containerBackground = {
+        backgroundImage: `url(${slides[currentSlide]})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        filter: 'blur(10px)',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: -1,
+    }
+
     return (
         <div className={css.container} >
+            <div style={containerBackground}></div>
             <div className={css.carouselInner} style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
                 {slides.map((slide, index) => (
                     <div className={css.carouselItem} key={index}>
