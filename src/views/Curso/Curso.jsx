8a51da348a363d73545345515f4a23c85bcd06fe
JUnzuetaLuza <joseunz2k@gmail.com>
2024-09-05 23:45:20 -0500

    feat(Curso): Base skeleton

--- src/views/Curso/Curso.jsx
+++ src/views/Curso/Curso.jsx
@@ -1,12 +1,31 @@
+import css from './Curso.module.css';
 import { NavBar } from '../../components/NavBar/NavBar'
 import { Footer } from '../../components/Footer/Footer'
+import persona from '../../assets/persona.png';
+import inspiration from '../../assets/inspiration.png';
+import think from '../../assets/think.png';
+
 
 export const Curso = () => {
 
   return (
     <>
       <NavBar/>
-      
+      <section className={css.firstModule}>
+        <div className={css.firstRight}>
+          <h2 className={css.title}>Curso</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reprehenderit pariatur cupiditate nemo mollitia similique dicta voluptatem rem officiis, error libero accusamus repellendus quis quaerat! Obcaecati, vitae? Quas obcaecati expedita amet dolore debitis rerum, asperiores labore harum blanditiis, alias cum. Dignissimos, commodi error. Suscipit perspiciatis veritatis, reprehenderit rerum, iure eligendi ducimus consequatur iusto fugiat voluptates doloremque necessitatibus illum praesentium est?</p>
+        </div>
+        <img src={persona} className={css.photo} />
+      </section>
+      <section className={css.firstModule}>
+        <img src={persona} className={css.photo} />
+        <div className={css.firstRight}>
+          <h2 className={css.title}>Curso</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reprehenderit pariatur cupiditate nemo mollitia similique dicta voluptatem rem officiis, error libero accusamus repellendus quis quaerat! Obcaecati, vitae? Quas obcaecati expedita amet dolore debitis rerum, asperiores labore harum blanditiis, alias cum. Dignissimos, commodi error. Suscipit perspiciatis veritatis, reprehenderit rerum, iure eligendi ducimus consequatur iusto fugiat voluptates doloremque necessitatibus illum praesentium est?</p>
+          <button className={css.btnCurso}>Curso</button>
+        </div>
+      </section>
       <Footer/>
     </>
   )
