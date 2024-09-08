5ef598984cd7922c6885f8a090200a5b031c5678
JUnzuetaLuza <joseunz2k@gmail.com>
2024-09-07 23:40:52 -0500

    feat(Curso): Finished view

--- src/views/Curso/Curso.jsx
+++ src/views/Curso/Curso.jsx
@@ -1,9 +1,9 @@
 import css from './Curso.module.css';
 import { NavBar } from '../../components/NavBar/NavBar'
 import { Footer } from '../../components/Footer/Footer'
-import persona from '../../assets/persona.png';
-import inspiration from '../../assets/inspiration.png';
-import think from '../../assets/think.png';
+import personamic from '../../assets/personamic.png';
+import persons from '../../assets/persons.png';
+import { Link } from 'react-router-dom';
 
 
 export const Curso = () => {
@@ -14,16 +14,16 @@ export const Curso = () => {
       <section className={css.firstModule}>
         <div className={css.firstRight}>
           <h2 className={css.title}>Curso</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reprehenderit pariatur cupiditate nemo mollitia similique dicta voluptatem rem officiis, error libero accusamus repellendus quis quaerat! Obcaecati, vitae? Quas obcaecati expedita amet dolore debitis rerum, asperiores labore harum blanditiis, alias cum. Dignissimos, commodi error. Suscipit perspiciatis veritatis, reprehenderit rerum, iure eligendi ducimus consequatur iusto fugiat voluptates doloremque necessitatibus illum praesentium est?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reprehenderit pariatur cupiditate nemo mollitia similique dicta voluptatem rem officiis, error libero accusamus repellendus quis quaerat! Obcaecati, vitae? Quas obcaecati expedita amet dolore debitis rerum, asperiores labore harum blanditiis, alias cum.<br/><br/> Dignissimos, commodi error. Suscipit perspiciatis veritatis, reprehenderit rerum, iure eligendi ducimus consequatur iusto fugiat voluptates doloremque necessitatibus illum praesentium est?</p>
         </div>
-        <img src={persona} className={css.photo} />
+        <img src={personamic} className={css.photo} />
       </section>
       <section className={css.firstModule}>
-        <img src={persona} className={css.photo} />
+        <img src={persons} className={css.photo} />
         <div className={css.firstRight}>
-          <h2 className={css.title}>Curso</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reprehenderit pariatur cupiditate nemo mollitia similique dicta voluptatem rem officiis, error libero accusamus repellendus quis quaerat! Obcaecati, vitae? Quas obcaecati expedita amet dolore debitis rerum, asperiores labore harum blanditiis, alias cum. Dignissimos, commodi error. Suscipit perspiciatis veritatis, reprehenderit rerum, iure eligendi ducimus consequatur iusto fugiat voluptates doloremque necessitatibus illum praesentium est?</p>
-          <button className={css.btnCurso}>Curso</button>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reprehenderit pariatur cupiditate nemo mollitia similique dicta voluptatem rem officiis, error libero accusamus repellendus quis quaerat! Obcaecati, vitae? Quas obcaecati expedita amet dolore debitis rerum, asperiores labore harum blanditiis, alias cum. <br/><br/>Dignissimos, commodi error. Suscipit perspiciatis veritatis, reprehenderit rerum, iure eligendi ducimus consequatur iusto fugiat voluptates doloremque necessitatibus illum praesentium est?</p>
+          <br/>
+          <Link to='/contacto' className={css.btnContacto}>Contacto</Link>
         </div>
       </section>
       <Footer/>
